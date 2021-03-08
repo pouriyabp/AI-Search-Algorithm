91684bedb67d156cd0c98e236b58a5863e622f7b
pouriya <pouriya1379@gmail.com>
2021-03-08 17:03:13 +0330

    add dls and ids search but have bug

--- IDS.java
+++ IDS.java
@@ -275,16 +275,6 @@ public class IDS {
     }
 
 
-
-
-
-
-
-
-
-
-
-
     //func for chek node (from BFS)
     public boolean isGoal(Node node){
         if (node.map.at(node.player.i,node.player.j).name == 'C'){
@@ -327,9 +317,66 @@ public class IDS {
     }
 
     public void search(Node intialNode){
-                
+        int i=1;
+        while (i<Integer.MAX_VALUE){
+
+            if (dlsSearch(intialNode,i)==1) {
+                break;
+            }
+            i++;
+        }
 
     }
+
+    public int dlsSearch(Node intialNode,int depth){
+        int depthWeAre=0;
+        Stack <Node> frontier= new Stack<Node>();
+        Hashtable<String, Boolean> inFrontier = new Hashtable<>();
+        Hashtable<String, Boolean> explored = new Hashtable<>();
+
+        if(isGoal(intialNode)){
+            result(intialNode);
+            return 1;
+        }
+        frontier.add(intialNode);
+        inFrontier.put(intialNode.hash(),true);
+
+        while (!frontier.isEmpty()){
+
+                Node temp = frontier.pop();
+                inFrontier.remove(temp.hash());
+                explored.put(temp.hash(),true);
+                ArrayList<Node> children = temp.successor();
+
+                if (depthWeAre<depth) {
+
+                    for (int i = 0; i < children.size(); i++) {
+                        if (!(inFrontier.containsKey(children.get(i).hash())) && !(explored.containsKey(children.get(i).hash()))) {
+                            if (isGoal(children.get(i))) {
+                                result(children.get(i));
+                                return 1;
+                            }
+
+                            frontier.push(children.get(i));
+                            inFrontier.put(children.get(i).hash(), true);
+
+                        }
+
+                    }
+                }else {
+                    return 0;
+                    }
+            depthWeAre+=1;
+
+
+        }
+        return 0;
+
+
+    }
+
+
+
 
 
     public static void main(String[] args) {
