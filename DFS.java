dc4f39556e3444559566aa446c8a338208092bdb
pouriya <pouriya1379@gmail.com>
2021-03-08 15:05:24 +0330

    add dfs search.

--- DFS.java
+++ DFS.java
@@ -280,15 +280,48 @@ public class DFS{
     }
 
 
+    public void search(Node intialNode){
+        //Node node = new Node(player, map,null,null);
+        Stack <Node> frontier= new Stack<Node>();
+        Hashtable<String, Boolean> inFrontier = new Hashtable<>();
+        Hashtable<String, Boolean> explored = new Hashtable<>();
 
+        if(isGoal(intialNode)){
+            result(intialNode);
+            return;
+        }
+        frontier.add(intialNode);
+        inFrontier.put(intialNode.hash(),true);
+        while (!frontier.isEmpty()){
+            Node temp = frontier.pop();
+            inFrontier.remove(temp.hash());
+            explored.put(temp.hash(),true);
+            ArrayList<Node> children = temp.successor();
+            for (int i = 0;i<children.size();i++){
+                if(!(inFrontier.containsKey(children.get(i).hash())) && !(explored.containsKey(children.get(i).hash()))) {
+                    if (isGoal(children.get(i))) {
+                        result(children.get(i));
+                        return;
+                    }
+                    frontier.push(children.get(i));
+                    inFrontier.put(children.get(i).hash(), true);
+                }
+
+
+
+            }
+
+        }
+
+    }
 
 
 
    public static void main(String[] args) {
        DFS dfs=new DFS();
        dfs.buildMap();
-
-        
+       Node node = new Node(dfs.player, dfs.map, null,null);
+       dfs.search(node);
     }
 
 
