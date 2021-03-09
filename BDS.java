d34c47ff55d9f160f27d32c681fbc0eebdb280d9
pouriya <pouriya1379@gmail.com>
2021-03-09 08:31:14 +0330

    add result function.

--- BDS.java
+++ BDS.java
@@ -1,4 +1,6 @@
-import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class BDS
 {
@@ -271,7 +273,84 @@ public class BDS
     }
 
 
+    //func for result (from BFS)
+    public void result(Node node){
+        Stack<Node> nodes = new Stack<Node>();
+        while (true){
+            nodes.push(node);
+            if(node.parentNode == null){
+                break;
+            }
+            else {
+                node = node.parentNode;
+            }
+        }
+        nodes.pop();
+        try {
+            FileWriter myWriter = new FileWriter("result.txt");
+            while (!nodes.empty()){
+                Node tempNode = nodes.pop();
+                String action = tempNode.priviousAction;
+                System.out.println(action+" "+tempNode.player.money+" "+tempNode.player.food);
+                myWriter.write(action+"\n");
+                //print visualized map for every movement
+                visualizer.printMap(tempNode.map, tempNode.player);
+            }
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    //func for chek node (from BFS)
+    public boolean isGoal(Node node){
+        if (node.map.at(node.player.i,node.player.j).name == 'C'){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
+
+
+
+
+
+    public void bfs(Node node){
+        Queue<Node> frontier = new LinkedList<Node>();
+        Hashtable<String, Boolean> inFrontier = new Hashtable<>();
+        Hashtable<String, Boolean> explored = new Hashtable<>();
+        if(isGoal(node)){
+            result(node);
+            return;
+        }
+        frontier.add(node);
+        inFrontier.put(node.hash(),true);
+        while (!frontier.isEmpty()){
+            Node temp = frontier.poll();
+            inFrontier.remove(temp.hash());
+            explored.put(temp.hash(),true);
+            ArrayList<Node> children = temp.successor();
+            for(int i = 0;i<children.size();i++){
+                if(!(inFrontier.containsKey(children.get(i).hash())) && !(explored.containsKey(children.get(i).hash()))) {
+                    if (isGoal(children.get(i))) {
+                        result(children.get(i));
+                        return;
+                    }
+                    frontier.add(children.get(i));
+                    inFrontier.put(children.get(i).hash(), true);
+                }
+            }
+        }
+    }
+
+    public void search(Node intialNode,Node endNode) {
+
+
+
+    }
 
 
 }
