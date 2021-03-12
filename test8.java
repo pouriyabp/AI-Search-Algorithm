2e200db36e5e7e8f75857f7e73f9fa548b1be7fa
pouriya <pouriya1379@gmail.com>
2021-03-12 14:41:59 +0330

    add test case

---
+++ test8.java
@@ -0,0 +1,74 @@
+public class test8 {
+
+    public static void main(String[] args) {
+
+        Visualizer visualizer = new Visualizer();
+        //indexes start from zero
+
+        Player player = new Player(2,0,100,100);
+        //indexes start from zero
+        Map map = new Map(3,3);
+        //initialization of the map
+        //G for normal ground
+        //S for swamp
+        //K for key
+        //C for castle
+        //L for Loot
+        //B for bandit
+        //W for wildAnimals
+        //P for bridge
+
+        map.addEntity(0,0,new BaseEntity('K'));
+
+        map.addEntity(0,1,new  BaseEntity('S'));
+        map.addEntity(0,2,new BaseEntity('S'));
+
+        map.addEntity(1,0,new Bandit(50));
+        map.addEntity(1,1,new Loot(51,51));
+        map.addEntity(1,2,new BaseEntity('C'));
+
+        map.addEntity(2,0,new BaseEntity('G'));
+
+        map.addEntity(2,1,new BaseEntity('S'));
+
+        map.addEntity(2,2,new BaseEntity('G'));
+
+        System.out.println("player position: "+player.i+" "+player.j);
+
+        //print method prints the map
+        map.print();
+        visualizer.printMap(map, player);
+        Node node = new Node(player,map,null,null,0);
+        System.out.println("BFS Search!");
+        BFS bfs = new BFS();
+        bfs.search(node);
+        System.out.println("------------------------------------------------------");
+        System.out.println("DFS Search!");
+        DFS dfs =new DFS();
+        dfs.search(node);
+
+        System.out.println("------------------------------------------------------");
+        System.out.println("DLS Search!");
+        IDS ids=new IDS();
+        ids.dlsSearch(node, 11);
+        System.out.println("------------------------------------------------------");
+        System.out.println("IDS Search!");
+        ids.search(node);
+        System.out.println("------------------------------------------------------");
+        System.out.println("BDS Search!");
+        BDS bds = new BDS();
+        //bds.search(node,player);
+
+        System.out.println("------------------------------------------------------");
+        System.out.println("BDS Search2!");
+        bds.search2(node,player);
+
+
+
+    }
+
+
+
+
+
+}
